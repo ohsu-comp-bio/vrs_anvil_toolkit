62ca41f202a831073099361ca752d3d1a9052cb8
Quinn Wai Wong <quinnwai.wong@gmail.com>
2024-01-25 16:00:30 -0800

    clean up

--- setup.sh
+++ setup.sh
@@ -2,16 +2,16 @@
 
 # download seqrepo data locally
 SEQREPO_ROOT=~/seqrepo
-echo "SEQREPO_ROOT=~/seqrepo" > .env
 mkdir $SEQREPO_ROOT
+
 # for local use
 seqrepo --root-directory $SEQREPO_ROOT pull -i 2016-08-28
-# # to use on Terra / with enough space
+# # TODO: to replace on Terra / with enough space
 # seqrepo --root-directory $SEQREPO_ROOT pull --update-latest
 
 # store new directory
 echo "SEQREPO_LATEST_DIR=$(
-    find ~/seqrepo -depth 1 -type d -exec stat -f '%m %N' {} + \
+    find $SEQREPO_ROOT -depth 1 -type d -exec stat -f '%m %N' {} + \
     | sort -rn \
     | head -n 1 \
     | cut -f2- -d' '
