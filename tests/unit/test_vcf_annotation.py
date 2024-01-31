6e77734fd5b95ee14645ef9da56f417229d3fc6f
wongq <wongq@metal-1-0.local>
2024-01-30 16:38:02 -0800

    updatest fixture

--- tests/unit/test_vcf_annotation.py
+++ tests/unit/test_vcf_annotation.py
@@ -7,16 +7,22 @@ from ga4gh.vrs.extras.vcf_annotation import VCFAnnotator
 TEST_DATA_DIR = "tests/data"
 
 @pytest.fixture
-def get_latest_seqrepo_dir():
-    return os.environ.get('SEQREPO') + "/latest"
+def seqrepo_dir():
+   with open(".env") as f:
+      for line in f:
+         # Ignore comments and empty lines
+         if line.strip() and not line.strip().startswith('#'):
+            key, value = line.strip().split('=', 1)
+            if key == "SEQREPO_ROOT":
+               return value + "/latest"
 
-def test_small_vcf_annotation():
+def test_small_vcf_annotation(seqrepo_dir):
     input_vcf = f"{TEST_DATA_DIR}/test_vcf_input.vcf"
     output_vcf = f"{TEST_DATA_DIR}/test_vcf_output.vcf.gz"
     output_vrs_pkl = f"{TEST_DATA_DIR}/test_vrs_objects.pkl"
-    expected_vrs_pkl = f"{TEST_DATA_DIR}/expected_vrs_objects.pkl"
+    expected_vrs_pkl = f"{TEST_DATA_DIR}/vrs_objects.pkl"
 
-    vcf_annotator = VCFAnnotator(seqrepo_root_dir=get_latest_seqrepo_dir())
+    vcf_annotator = VCFAnnotator(seqrepo_root_dir=seqrepo_dir)
     vcf_annotator.annotate(input_vcf, output_vcf, output_vrs_pkl)
     
     try:
