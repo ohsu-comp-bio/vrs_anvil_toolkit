fb4df6300eb3d6e94ef8db4d50c0079f960f2dbb
Quinn Wai Wong <quinnwai.wong@gmail.com>
2024-02-01 12:23:02 -0800

    update test to check vcf line by line

--- tests/unit/test_vcf_annotation.py
+++ tests/unit/test_vcf_annotation.py
@@ -1,9 +1,11 @@
-import filecmp
 import os
 import pytest
+import vcf
 
 from ga4gh.vrs.extras.vcf_annotation import VCFAnnotator
 
+VRS_ERROR_FIELD = VCFAnnotator.VRS_ERROR_FIELD
+VRS_ALLELE_IDS_FIELD = VCFAnnotator.VRS_ALLELE_IDS_FIELD
 TEST_DATA_DIR = "tests/data"
 
 @pytest.fixture
@@ -16,17 +18,50 @@ def seqrepo_dir():
             if key == "SEQREPO_ROOT":
                return value + "/latest"
 
-def test_small_vcf_annotation(seqrepo_dir):
-    input_vcf = f"{TEST_DATA_DIR}/test_vcf_input.vcf"
-    output_vcf = f"{TEST_DATA_DIR}/test_vcf_output.vcf.gz"
-    output_vrs_pkl = f"{TEST_DATA_DIR}/test_vrs_objects.pkl"
-    expected_vrs_pkl = f"{TEST_DATA_DIR}/vrs_objects.pkl"
+@pytest.fixture(scope='function')
+def delete_output(request):
+   # cleanup output file after test execution
+   def cleanup():
+      os.remove(request.node.output_vcf)
 
-    vcf_annotator = VCFAnnotator(seqrepo_root_dir=seqrepo_dir)
-    vcf_annotator.annotate(input_vcf, output_vcf, output_vrs_pkl)
-    
-    try:
-        assert filecmp.cmp(output_vrs_pkl, expected_vrs_pkl), "files not identical"
-    finally:
-        os.remove(output_vcf)
-        os.remove(output_vrs_pkl)
+   request.addfinalizer(cleanup)
+
+def test_small_vcf_annotation(seqrepo_dir, delete_output, request):
+   
+   # create annotated vcf test file 
+   input_vcf = f"{TEST_DATA_DIR}/test_vcf_input.vcf"
+   expected_vcf = f"{TEST_DATA_DIR}/test_vcf_output.vcf"
+   output_vcf = f"{TEST_DATA_DIR}/test_vcf_output.vcf"
+   request.node.output_vcf = output_vcf
+
+   vcf_annotator = VCFAnnotator(seqrepo_root_dir=seqrepo_dir) 
+   vcf_annotator.annotate(input_vcf, output_vcf)
+
+   # assert ids are the same line by line
+   test_vcf_reader = vcf.Reader(open(output_vcf, 'r'))
+   expected_vcf_reader = vcf.Reader(open(expected_vcf, 'r'))
+   
+   for i, expected_record in enumerate(expected_vcf_reader):
+      try:
+         test_record = next(test_vcf_reader)
+      except:
+         assert False, "missing variants, not enough rows to compare to"
+         
+      variant_str = f"{i+1}th variant with record {test_record}"
+
+      # check successful annotation
+      # nice to have: output entire error, as pyvcf delimits with space
+      assert VRS_ERROR_FIELD not in test_record.INFO, \
+         f"VRS ERROR: unable to annotate {variant_str}, please check it"
+      
+      # check each ID matches
+      test_ids = test_record.INFO[VRS_ALLELE_IDS_FIELD]
+      expected_ids = expected_record.INFO[VRS_ALLELE_IDS_FIELD]
+
+      if isinstance(expected_ids, list):
+         assert isinstance(test_ids, list) and len(expected_ids) == len(test_ids), \
+            f"not the same number of IDs for {variant_str}"
+
+         for expected_id, test_id in zip(expected_ids, test_ids):
+            assert expected_id == test_id, \
+               f"ids for {variant_str} do not match"
