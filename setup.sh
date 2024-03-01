d9567f1f4d5c9253f140c7e4ebec19c94ca8a128
Quinn Wai Wong <quinnwai.wong@gmail.com>
2024-02-29 16:24:46 -0800

    update setup file for bcftools

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # create python package
-python -m venv venv
-source venv/bin/activate
+python3.10 -m venv venv3.10
+source venv3.10/bin/activate
 pip install -r requirements.txt
 
 # # download seqrepo data locally
 SEQREPO_ROOT=~/seqrepo
+BCFTOOLS_DIR=~/bcftools/
 echo "SEQREPO_ROOT=$SEQREPO_ROOT" > .env
 mkdir $SEQREPO_ROOT
 seqrepo --root-directory $SEQREPO_ROOT pull --update-latest
@@ -14,12 +15,12 @@ seqrepo --root-directory $SEQREPO_ROOT pull --update-latest
 # setup bcftools
 cd ~
 curl -LJO https://github.com/samtools/bcftools/releases/download/1.19/bcftools-1.19.tar.bz2
-BCF_TOOLS_TAR=$(ls -1t bcftools*.tar.bz2 | head -n 1)
-tar -xvf $BCF_TOOLS_TAR
-rm $BCF_TOOLS_TAR
+BCFTOOLS_TAR=$(ls -1t bcftools*.tar.bz2 | head -n 1)
+tar -xvf $BCFTOOLS_TAR
+rm $BCFTOOLS_TAR
 
-mv ~/bcftools*/ $BCF_TOOLS_DIR 
-cd $BCF_TOOLS_DIR
+mv ~/bcftools*/ $BCFTOOLS_DIR 
+cd $BCFTOOLS_DIR
 ./configure prefix=$HOME
 make
 make install
