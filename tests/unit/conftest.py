9269aa7cf03e76b07a4f4bb448c4a35a520af87e
Quinn Wai Wong <54592956+quinnwai@users.noreply.github.com>
2024-03-06 14:38:59 -0800

    Add 1000G test for small vcf (#17)
    * test 1000g on terra
    
    * relative import change
    
    * diskcache requirements

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,15 +1,19 @@
+import json
+import pathlib
 from concurrent.futures.thread import ThreadPoolExecutor
 from concurrent.futures import as_completed
 import queue
 import traceback
 from functools import lru_cache
 from threading import current_thread
-from typing import Optional
+from typing import Optional, Generator
 
 import pytest
 from biocommons.seqrepo import SeqRepo
+from diskcache import Cache
 from ga4gh.vrs.dataproxy import SeqRepoDataProxy
 from ga4gh.vrs.extras.translator import AlleleTranslator
+# from glom import glom
 from pydantic import BaseModel
 
 
@@ -101,3 +105,88 @@ def my_translator():
 def threaded_translator():
     """Return a "thread manager", a pool of translator instances."""
     return ThreadedTranslator(normalize=False)
+
+
+def generate_gnomad_id(vcf_line) -> list[str]:
+    """Assuming a standard VCF format with tab-separated fields, generate a gnomAD-like ID from a VCF line."""
+    # TODO - Change the way we generate the gnomad_id to match the way it is done in the vcf_annotation.py see https://github.com/ga4gh/vrs-python/blob/main/src/ga4gh/vrs/extras/vcf_annotation.py#L386-L411
+    fields = vcf_line.strip().split('\t')
+
+    # Extract relevant information (you may need to adjust these indices based on your VCF format)
+    chromosome = fields[0]
+    position = fields[1]
+    reference_allele = fields[3]
+    alternate_allele = fields[4]
+
+    # Create a gnomAD-like ID
+    gnomad_id = f"{chromosome}-{position}-{reference_allele}-{alternate_allele}"
+
+    return gnomad_id
+
+
+@pytest.fixture()
+def num_threads():
+    """Return the number of threads to use for testing."""
+    return 20
+
+
+def params_from_vcf(path, limit=None) -> Generator[dict, None, None]:
+    """Open the vcf file, skip headers, yield the first lines as gnomad-like IDs"""
+    c = 0
+    with open(path, "r") as f:
+        for line in f:
+            if line.startswith("#"):
+                continue
+            gnomad_id = generate_gnomad_id(line)
+            yield {"fmt": "gnomad", "var": gnomad_id}
+            c += 1
+            if limit and c > limit:
+                break
+
+
+@pytest.fixture
+def thousand_genome_vcf() -> pathlib.Path:
+    """Return a path to a 1000g vcf."""
+    _ = pathlib.Path("tests/data/1kGP.chr1.1000.vcf")
+    assert _.exists()
+    return _
+
+
+def find_items_with_key(dictionary, key_to_find):
+    """Find all items in a dictionary that have a specific key."""
+    result = glom(dictionary, f"**.{key_to_find}")
+    return result
+
+
+def meta_kb_ids(metakb_path) -> Generator[str, None, None]:
+    """Find all the applicable vrs ids in the metakb files."""
+    for file_name in pathlib.Path(metakb_path).glob("*.json"):
+        if file_name.is_file():
+            with open(file_name, 'r') as file:
+                data = json.loads(file.read())
+                yield from ([_ for _ in find_items_with_key(data, 'id') if _.startswith('ga4gh:VA')])
+
+
+class MetaKBProxy(BaseModel):
+    """A proxy for the MetaKB, maintains a cache of VRS ids."""
+    metakb_path: pathlib.Path
+    _cache: Optional[Cache] = None
+
+    def __init__(self, metakb_path: pathlib.Path, cache: Cache = None):
+        super().__init__(metakb_path=metakb_path, _cache=cache)
+        if cache is None:
+            reload_cache = False
+            if not (metakb_path / 'cache').is_dir():
+                reload_cache = True
+            cache = Cache(directory=metakb_path / 'cache')
+            cache.stats(enable=True)
+            if reload_cache:
+                for _ in meta_kb_ids(metakb_path):
+                    cache.set(_, True)
+            print(cache.stats())
+        self._cache = cache
+
+    def get(self, vrs_id: str) -> bool:
+        """Get the vrs_id from the cache."""
+        return self._cache.get(vrs_id, False)
+
