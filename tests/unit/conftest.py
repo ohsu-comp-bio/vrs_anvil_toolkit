41c9b1c05835cd425a1b8df0039b3ac9fa776b45
Brian Walsh <brian@bwalsh.com>
2024-03-07 07:16:17 -0800

    Improve semantics of manifest initialization

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -14,33 +14,49 @@ _logger.setLevel(logging.DEBUG)
 
 
 @pytest.fixture
-def testing_manifest():
-    """Return a testing manifest."""
-    with open(pathlib.Path("tests/fixtures/manifest.yaml"), 'r') as stream:
+def manifest_path() -> pathlib.Path:
+    """Return a path to a manifest file."""
+    _ = pathlib.Path("tests/fixtures/manifest.yaml")
+    assert _.exists()
+    return _
+
+
+@pytest.fixture
+def testing_manifest(manifest_path: pathlib.Path) -> Manifest:
+    """Set the testing manifest for this run."""
+    with open(manifest_path, 'r') as stream:
         manifest = Manifest.parse_obj(yaml.safe_load(stream))
-        vrs_anvil.manifest = manifest
+    return manifest
 
 
 @pytest.fixture
-def seqrepo_dir():
+def initialized_manifest(testing_manifest) -> bool:
+    """state change, the manifest is initialized."""
+    vrs_anvil.manifest = testing_manifest
+    return True
+
+
+@pytest.fixture
+def seqrepo_dir(testing_manifest):
     """Return the seqrepo directory as fixture."""
-    return _seqrepo_dir()
+    return testing_manifest.seqrepo_directory
 
 
 @pytest.fixture
-def my_translator(testing_manifest):
+def my_translator(initialized_manifest):
     """Return a single translator instance."""
+    # relies on testing_manifest setting the vrs_anvil.manifest
     return caching_allele_translator_factory()
 
 
 @pytest.fixture
-def threaded_translator(testing_manifest):
+def threaded_translator(initialized_manifest):
     """Return a "thread manager", a pool of translator instances."""
     return ThreadedTranslator(normalize=False)
 
 
 @pytest.fixture()
-def num_threads():
+def num_threads(testing_manifest):
     """Return the number of threads to use for testing."""
     return vrs_anvil.manifest.num_threads
 
@@ -49,14 +65,6 @@ def num_threads():
 def thousand_genome_vcf() -> pathlib.Path:
     """Return a path to a 1000g vcf."""
     _ = pathlib.Path("tests/fixtures/1kGP.chr1.1000.vcf")
-    assert _.exists()
-    return _
-
-
-@pytest.fixture
-def manifest_path() -> pathlib.Path:
-    """Return a path to a manifest file."""
-    _ = pathlib.Path("tests/fixtures/manifest.yaml")
     assert _.exists()
     return _
 
