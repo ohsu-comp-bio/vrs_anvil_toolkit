691ae9c4b1848622003c17b179d677cc69df76fd
quinnwai <quinnwai.wong@gmail.com>
2024-03-21 16:39:07 -0700

    update tests for metakb cache

--- tests/unit/test_metakb.py
+++ tests/unit/test_metakb.py
@@ -7,20 +7,34 @@ from vrs_anvil import metakb_ids, MetaKBProxy
 
 @pytest.fixture
 def expected_vrs_id_count():
-    return 2970
+    return 2986
 
 
-def test_metakb_ids(metakb_directory, expected_vrs_id_count):
+@pytest.fixture
+def expected_vrs_ids():
+    return [
+        "ga4gh:VA.SOEVGpU16hxYQtJNeRyfq0V-B0rSOGK-",
+        "ga4gh:VA.fU8g-a8s0n-tFsj3-XbsuFe17MfySB4q",
+        "ga4gh:VA.rRPCnh0XXjuePRGWerw6PhVXFYjhchwP",
+    ]
+
+
+def test_metakb_ids(metakb_directory, expected_vrs_id_count, expected_vrs_ids):
+    # TODO: doesn't work with calling pytest tests/unit/test_metakb
+    # as it cannot find the manifest
     """Test metakb ids."""
-    vrs_ids = [_ for _ in metakb_ids(metakb_directory)]
+    vrs_ids = [vrs_id for vrs_id in metakb_ids(metakb_directory)]
     vrs_count = len(vrs_ids)
     assert (
         vrs_count >= expected_vrs_id_count
     ), f"Not enough VRS ids found in metakb {vrs_count} {vrs_ids}"
     metakb_proxy = MetaKBProxy(metakb_path=pathlib.Path(metakb_directory))
 
-    for _ in vrs_ids:
-        assert metakb_proxy.get(_), f"VRS id {_} not found in cache {_}"
+    for id in vrs_ids:
+        assert metakb_proxy.get(id), f"VRS id {id} not found in cache {id}"
 
-    hits, misses = metakb_proxy._cache.stats()
+    for id in expected_vrs_ids:
+        assert metakb_proxy.get(id), f"Expected VRS id {id} not found in cache {id}"
+
+    _, misses = metakb_proxy._cache.stats()
     assert misses == 0, f"Misses found in cache {misses}"
