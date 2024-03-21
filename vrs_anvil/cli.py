eca48d1903f80e4d8e43e4f8cd1da246ba087ac8
quinnwai <quinnwai.wong@gmail.com>
2024-03-21 14:01:08 -0700

    linting changes

--- vrs_anvil/cli.py
+++ vrs_anvil/cli.py
@@ -1,4 +1,3 @@
-
 import click
 import yaml
 import logging
@@ -15,10 +14,15 @@ _logger = logging.getLogger("vrs_anvil.cli")
 
 
 @click.group(invoke_without_command=True)
-@click.version_option(package_name='vrs_anvil')
-@click.option('--manifest', type=click.Path(exists=False), default='manifest.yaml', help='Path to manifest file.')
-@click.option('--verbose', default=False, help='Log more information', is_flag=True)
-@click.option('--max_errors', default=10, help='Number of acceptable errors.')
+@click.version_option(package_name="vrs_anvil")
+@click.option(
+    "--manifest",
+    type=click.Path(exists=False),
+    default="manifest.yaml",
+    help="Path to manifest file.",
+)
+@click.option("--verbose", default=False, help="Log more information", is_flag=True)
+@click.option("--max_errors", default=10, help="Number of acceptable errors.")
 @click.pass_context
 def cli(ctx, verbose: bool, manifest: str, max_errors: int):
     """GA4GH GKS utility for AnVIL."""
@@ -27,52 +31,58 @@ def cli(ctx, verbose: bool, manifest: str, max_errors: int):
     if verbose:
         _log_level = logging.DEBUG
     try:
-        with open(manifest, 'r') as stream:
+        with open(manifest, "r") as stream:
             manifest = Manifest.model_validate(yaml.safe_load(stream))
 
             # Create a rotating file handler with a max size of 10MB and keep 3 backup files
             log_path = pathlib.Path(manifest.state_directory) / "vrs_anvil.log"
             file_handler = RotatingFileHandler(
-                    log_path, maxBytes=10 * 1024 * 1024, backupCount=3)
+                log_path, maxBytes=10 * 1024 * 1024, backupCount=3
+            )
             file_handler.setLevel(_log_level)
             file_handler.setFormatter(logging.Formatter(log_format))
 
             # Add the file handler to the logger
             # logger.addHandler(file_handler)
             # basicConfig call removed, which prevents the default configuration that logs to the console.
-            logging.basicConfig(level=_log_level, format=log_format, handlers=[file_handler])
+            logging.basicConfig(
+                level=_log_level, format=log_format, handlers=[file_handler]
+            )
 
-            click.secho(f"🪵  Logging to {log_path}, level {logging.getLevelName(_log_level)}", fg='yellow')
+            click.secho(
+                f"🪵  Logging to {log_path}, level {logging.getLevelName(_log_level)}",
+                fg="yellow",
+            )
 
             ctx.ensure_object(dict)
-            ctx.obj['manifest'] = manifest
-            ctx.obj['verbose'] = verbose
-            ctx.obj['max_errors'] = max_errors
+            ctx.obj["manifest"] = manifest
+            ctx.obj["verbose"] = verbose
+            ctx.obj["max_errors"] = max_errors
 
             if verbose:
-                click.secho(f"📢  {manifest}", fg='green')
+                click.secho(f"📢  {manifest}", fg="green")
 
     except Exception as exc:
-        click.secho(f"{exc}", fg='yellow')
+        click.secho(f"{exc}", fg="yellow")
         ctx.ensure_object(dict)
 
 
-@cli.command('annotate')
+@cli.command("annotate")
 @click.pass_context
 def annotate_cli(ctx):
     """Read manifest file, annotate variants, all parameters controlled by manifest.yaml."""
 
     try:
-        assert 'manifest' in ctx.obj, "Manifest not found."
-        manifest = ctx.obj['manifest']
+        assert "manifest" in ctx.obj, "Manifest not found."
+        manifest = ctx.obj["manifest"]
         _logger.debug(f"Manifest: {ctx.obj['manifest']}")
-        click.secho("🚧  annotating variants", fg='yellow')
-        metrics_file = annotate_all(manifest, max_errors=ctx.obj['max_errors'])
-        click.secho(f"📊  metrics available in {metrics_file}", fg='green')
+        click.secho("🚧  annotating variants", fg="yellow")
+        metrics_file = annotate_all(manifest, max_errors=ctx.obj["max_errors"])
+        click.secho(f"📊  metrics available in {metrics_file}", fg="green")
     except Exception as exc:
-        click.secho(f"{exc}", fg='red')
+        click.secho(f"{exc}", fg="red")
         _logger.exception(exc)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
