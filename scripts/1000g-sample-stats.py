9a90ecbf9009af54d1e2ed09154b8c038329c9f0
quinnwai <quinnwai.wong@gmail.com>
2024-03-28 13:18:24 -0700

    histograms split by variant type

--- scripts/1000g-sample-stats.py
+++ scripts/1000g-sample-stats.py
@@ -48,7 +48,7 @@ evidence_per_file = {}
 
 # collect all evidence from a list of metrics files
 # chrs = [str(num) for num in range(1,23)] + ["X"]
-chrs = [str(num) for num in range(1,3)]
+chrs = [str(num) for num in range(1, 3)]
 for c in chrs:
     all_metrics_paths = glob(f"all-chrs/chr{c}/state/metrics*.yaml")
     most_recent_metrics_path = all_metrics_paths[-1]
@@ -73,9 +73,11 @@ for c in chrs:
             print("no evidence from this file")
         print()
 
-# collect info for each vrs allele id
+# get study info (id: description) for each variant
 for file_path, evidence in evidence_per_file.items():
     vcf_reader = pysam.VariantFile(file_path)
+
+    print(truncate(file_path, 0, 47))
 
     for allele_id, allele_info in evidence.items():
 
@@ -88,26 +90,33 @@ for file_path, evidence in evidence_per_file.items():
 
         # get study id associated with vrs allele id
         metakb_response = query_metakb(allele_id, log=True)
-        assert metakb_response is not None, f"no metakb hit for allele {allele_id}"
+        if metakb_response is None:
+            print(f"no metakb hit for allele {allele_id}\n")
+            continue
         study_ids = metakb_response["study_ids"]
 
-        assert study_ids == [study["id"] for study in metakb_response["studies"]], \
-            "study ids in wrong order to add variant types"
-        variant_types = [study["qualifiers"]["alleleOrigin"] for study in metakb_response["studies"]]
+        assert study_ids == [
+            study["id"] for study in metakb_response["studies"]
+        ], "study ids in wrong order to add variant types"
+        variant_types = [
+            study["qualifiers"]["alleleOrigin"] for study in metakb_response["studies"]
+        ]
         variant_types_set.update(variant_types)
 
         for study in metakb_response["studies"]:
             print(f"\t{study['type']} ({study['id']}): {study['description']}")
 
         # should be a single record each time
-        assert (
-            sum([1 for _ in vcf_reader.fetch(chrom, pos - 1, pos)]) == 1
-        ), f"more than one record found at pos {pos}"
+        if sum([1 for _ in vcf_reader.fetch(chrom, pos - 1, pos)]) != 1:
+            print(f"[WARNING] more than one record found at pos {pos}")
 
         for record_idx, record in enumerate(vcf_reader.fetch(chrom, pos - 1, pos)):
 
             assert ref == record.ref, f"expected ref {record.ref}, got {ref}"
-            assert alt in record.alts, f"expected one of {record.alts} alts, got {alt}"
+            if alt not in record.alts:
+                print(
+                    f"Skipping {gnomad_expr}, expected one of {record.alts} alts, got {alt}"
+                )
 
             # populate patient/sample dictionary
             id_count = 0
@@ -154,7 +163,6 @@ dtypes = [str, float, str]
 
 TOTAL = "all"
 variants = sorted(list(variant_types_set)) + [TOTAL]
-
 data = None
 
 # for each variant type (germline, somatic) and kb, get count of people in
@@ -173,7 +181,6 @@ for variant_type in variants:
             if any(keyword in study_id for study_id in study_ids):
                 num_matching_samples += 1
 
-
         percent = num_matching_samples * 100.0 / num_samples
         if data is None:
             data = np.array([knowledgebases[i], percent, variant_type])
@@ -181,27 +188,39 @@ for variant_type in variants:
             data = np.vstack([data, [knowledgebases[i], percent, variant_type]])
 
 expected_num_rows = len(knowledgebases) * len(variants)
-assert len(data) == expected_num_rows, f"expected {expected_num_rows} rows, got {len(data)}"
+assert (
+    len(data) == expected_num_rows
+), f"expected {expected_num_rows} rows, got {len(data)}"
 
 # bar plot
-df_pct = pd.DataFrame(data, columns=cols).astype(dtype={col: dtype for col, dtype in zip(cols, dtypes)})
-ax = sns.barplot(data=df_pct, x=cols[VAR], y=cols[PCT], hue=cols[KB], order=["somatic", "germline", TOTAL])
+df_pct = pd.DataFrame(data, columns=cols).astype(
+    dtype={col: dtype for col, dtype in zip(cols, dtypes)}
+)
+ax = sns.barplot(
+    data=df_pct,
+    x=cols[VAR],
+    y=cols[PCT],
+    hue=cols[KB],
+    order=["somatic", "germline", TOTAL],
+)
+
 
 # add percent labels
-def add_percent_labels(df, ax, vertical_nudge, from_decimal=False):
+def add_percent_labels(num_bars, ax, vertical_nudge, from_decimal=False):
     multiplier = 100 if from_decimal else 1
 
     for i, bar in enumerate(ax.patches):
-        if i >= len(df):
+        if i >= num_bars:
             break
 
         height = bar.get_height()
         percent = f"{multiplier * height:.2f}"
         x = bar.get_x() + bar.get_width() / 2
         y = bar.get_y() + height + vertical_nudge
-        ax.text(x, y, percent, ha='center', va='center', fontsize=10)
+        ax.text(x, y, percent, ha="center", va="center", fontsize=10)
 
-add_percent_labels(df_pct, ax, 2.5)
+
+add_percent_labels(len(df_pct), ax, 2.5)
 
 # sns.despine(left=True)
 plt.ylim(0, 100)
@@ -214,7 +233,7 @@ plt.title(
     "[chr1 to chr2] Percent of 1000G Patients with a Variant Match (n=3202)",
     wrap=True,
 )
-plt.legend(loc='upper left')
+plt.legend(loc="upper left")
 
 # Show the plot
 plt.tight_layout()
@@ -225,26 +244,50 @@ if show_figures:
     plt.show()
 
 # average number of variants for all samples
-num_variants_per_patient = [len(v["vrs_ids"]) for v in sample_dict.values()]
-num_variants_per_patient.extend([0 for _ in range(num_samples - len(sample_dict))])
+num_variant_hits = sum(len(e) for e in evidence_per_file.values())
+print("total hits across all patients:", num_variant_hits)
 
-plt.figure()
+jitter = [0.008, 0.018, 0.004]
+
+
 NUM_VARIANTS = "num_variants"
 
-df = pd.DataFrame({NUM_VARIANTS: num_variants_per_patient})
-df["percentage"] = df[NUM_VARIANTS].value_counts(normalize=True) * 100
-ax = sns.histplot(data=df, x=NUM_VARIANTS, stat="density", discrete=True)
-plt.xlabel("Number of Variants")
-plt.ylabel("Percentage of All Patients (%)")
-plt.title("Number of Variants Associated with Each Patient")
+for i, variant_type in enumerate(variants):
+    num_variants_per_patient = [
+        len(
+            [
+                v
+                for v in values["variant_types"]
+                if v == variant_type or variant_type == TOTAL
+            ]
+        )
+        for values in sample_dict.values()
+    ]
 
-add_percent_labels(df, ax, 0.01, from_decimal=True)
+    num_variants_per_patient.extend([0 for _ in range(num_samples - len(sample_dict))])
+    assert (
+        len(num_variants_per_patient) == num_samples
+    ), f"only {len(num_variants_per_patient)} samples, expected {num_samples}"
 
-plt.gca().yaxis.set_major_formatter(lambda x, _: f"{(x*100):.0f}")
-plt.grid(False)
-plt.xticks(range(min(df[NUM_VARIANTS]), max((df[NUM_VARIANTS])+1)))
+    df = pd.DataFrame({NUM_VARIANTS: num_variants_per_patient})
+    df["percentage"] = df[NUM_VARIANTS].value_counts(normalize=True) * 100
 
-if save_figures:
-    plt.savefig(f"{figure_dir}/{variant_histogram_file_name}", dpi=300)
-if show_figures:
-    plt.show()
+    plt.figure()
+    ax = sns.histplot(data=df, x=NUM_VARIANTS, stat="density", discrete=True)
+    plt.xlabel("Number of Variants")
+    plt.ylabel("Percentage of All Patients (%)")
+    plt.title(
+        f"Number of {variant_type.capitalize()} Variants Associated with Each Patient"
+    )
+
+    variant_hits_by_type = max((df[NUM_VARIANTS]) + 1)
+    add_percent_labels(variant_hits_by_type, ax, jitter[i], from_decimal=True)
+
+    plt.gca().yaxis.set_major_formatter(lambda x, _: f"{(x*100):.0f}")
+    plt.grid(False)
+    plt.xticks(range(min(df[NUM_VARIANTS]), variant_hits_by_type))
+
+    if save_figures:
+        plt.savefig(f"{figure_dir}/{variant_histogram_file_name}", dpi=300)
+    if show_figures:
+        plt.show()
