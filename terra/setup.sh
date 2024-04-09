d61cf0f42412dad32fe0f1d2d443b30d3e6eb6eb
quinnwai <quinnwai.wong@gmail.com>
2024-04-09 12:14:14 -0700

    remove bcftools from setup

--- terra/setup.sh
+++ terra/setup.sh
@@ -7,20 +7,3 @@ SEQREPO_ROOT=~/seqrepo
 BCFTOOLS_DIR=~/bcftools/
 mkdir $SEQREPO_ROOT
 seqrepo --root-directory $SEQREPO_ROOT pull --update-latest
-
-# setup bcftools
-if [ bcftools --version &> /dev/null ]; then
-    cd ~
-    curl -LJO https://github.com/samtools/bcftools/releases/download/1.19/bcftools-1.19.tar.bz2
-    BCFTOOLS_TAR=$(ls -1t bcftools*.tar.bz2 | head -n 1)
-    tar -xvf $BCFTOOLS_TAR
-    rm $BCFTOOLS_TAR
-
-    mv ~/bcftools*/ $BCFTOOLS_DIR
-    cd $BCFTOOLS_DIR
-    ./configure prefix=$HOME
-    make
-    make install
-else
-    echo "bcftools already installed, not installing"
-fi
