ace0cb73e5030509d4da3b7ac484c772abed96e7
quinnwai <quinnwai.wong@gmail.com>
2024-07-11 12:18:33 -0700

    new script

--- terra/setup.sh
+++ terra/setup.sh
@@ -6,8 +6,10 @@ pip install -r requirements.txt
 SEQREPO_ROOT="$HOME/seqrepo"
 SEQREPO_TARBALL="seqrepo.tar.gz"
 
-mkdir $SEQREPO_ROOT
 gsutil cp "$WORKSPACE_BUCKET/$SEQREPO_TARBALL" $HOME
-tar -xzf "$HOME/$SEQREPO_TARBALL"
+echo "creating seqrepo from tar..."
+tar -xzf "$HOME/$SEQREPO_TARBALL" --directory=$HOME
 seqrepo --root-directory $SEQREPO_ROOT update-latest
+echo "removing tarball..."
 rm "$HOME/$SEQREPO_TARBALL"
+echo "done"
