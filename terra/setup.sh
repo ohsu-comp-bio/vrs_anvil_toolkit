bf25ed0ec6ea329ffbe3944f50171a746ca77be7
quinnwai <quinnwai.wong@gmail.com>
2024-07-11 12:04:16 -0700

    update terra setup

--- terra/setup.sh
+++ terra/setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# setup python packages and dependencies
 pip install -r requirements.txt
 
-SEQREPO_ROOT=~/seqrepo
+# setup reference sequence database (seqrepo)
+SEQREPO_ROOT="$HOME/seqrepo"
+SEQREPO_TARBALL="seqrepo.tar.gz"
+
 mkdir $SEQREPO_ROOT
-seqrepo --root-directory $SEQREPO_ROOT pull --update-latest
+gsutil cp "$WORKSPACE_BUCKET/$SEQREPO_TARBALL" $HOME
+tar -xzf "$HOME/$SEQREPO_TARBALL"
+seqrepo --root-directory $SEQREPO_ROOT update-latest
+rm "$HOME/$SEQREPO_TARBALL"
